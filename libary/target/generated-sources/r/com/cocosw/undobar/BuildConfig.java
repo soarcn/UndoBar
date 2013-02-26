be4e7b64359a47a42ba8c7ef54241db7c1d75603
soar <soarcn@gmail.com>
2013-02-26 17:00:53 +0800

    merge maven-release

--- libary/target/generated-sources/r/com/cocosw/undobar/BuildConfig.java
+++ libary/target/generated-sources/r/com/cocosw/undobar/BuildConfig.java
@@ -1,6 +1,5 @@
-/** Automatically generated file. DO NOT MODIFY */
 package com.cocosw.undobar;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+  public static final boolean DEBUG = true;
 }
